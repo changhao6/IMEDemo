a1c8e47f1f7e4b1bdfefa3ce09eb579555afb3bf
Chang Hao <luca.chang@developertools.com>
2023-12-24 23:57:31 +0800

    upload demo

---
+++ Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace IME
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myTextBox1 = new IME.MyTextBox();
+            this.SuspendLayout();
+            // 
+            // myTextBox1
+            // 
+            this.myTextBox1.Location = new System.Drawing.Point(93, 102);
+            this.myTextBox1.Name = "myTextBox1";
+            this.myTextBox1.Size = new System.Drawing.Size(191, 28);
+            this.myTextBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.myTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MyTextBox myTextBox1;
+    }
+}
+
---
+++ MyTextBox.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IME
+{
+    internal class MyTextBox : TextBox
+    {
+
+        const int GCS_RESULTREADSTR = 0x0200;
+        public const int WM_IME_COMPOSITION = 0x010F;
+        public const int WM_IME_ENDCOMPOSITION = 0x010E;
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_IME_ENDCOMPOSITION)
+            {
+                MessageBox.Show(GetCompositionString(GCS_RESULTREADSTR));
+            }
+            base.WndProc(ref m);
+        }
+
+        string GetCompositionString(uint dwIndex)
+        {
+
+            IntPtr hWnd = this.Handle;
+
+            IntPtr hIMC = ImmGetContext(hWnd);
+
+            int dwSize = ImmGetCompositionString(hIMC, dwIndex, null, 0);
+
+            if (dwSize <= 0)
+            {
+                return string.Empty;
+            }
+
+            string result = new string('\0', dwSize);
+            ImmGetCompositionString(hIMC, dwIndex, result, dwSize);
+
+            return result.ToString().TrimEnd('\0');
+        }
+
+        [DllImport("imm32.dll", CharSet = CharSet.Unicode)]
+        static extern int ImmGetCompositionString(IntPtr hIMC, uint dwIndex, string lpBuf, int dwBufLen);
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+        [DllImport("imm32.dll")]
+        public static extern IntPtr ImmGetContext(IntPtr hWnd);
+    }
+}
